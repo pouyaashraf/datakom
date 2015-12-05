6c7b21b3db297736fb194d4aa0d32ba9a600dfc9
pouyaashraf <pouya.ashraf@gmail.com>
2015-12-05 16:14:04 +0100

    -m pushing indented files

--- Main.java
+++ Main.java
@@ -10,28 +10,25 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
-	//public static ArrayList<RunNode> nodes;
-	public static DatagramSocket server = null;
-	
-	public static void main(String[] args) {
-		int port = 1549;
-		ExecutorService executor = Executors.newFixedThreadPool(5);
-		for (int i = 0; i < 3 /*cookies*/; i++) {
-			creatNode(port + i, executor);
-		}
-	  	
-	      executor.shutdown();
-	      while (!executor.isTerminated()) {
-	      }
-	      System.out.println("Finished all threads");
-
+    //public static ArrayList<RunNode> nodes;
+    public static DatagramSocket server = null;
+    
+    public static void main(String[] args) {
+	int port = 1549;
+	ExecutorService executor = Executors.newFixedThreadPool(5);
+	for (int i = 0; i < 3 /*cookies*/; i++) {
+	    creatNode(port + i, executor);
 	}
-
-	private static void creatNode(int port, ExecutorService exec) {
-		        Runnable worker = new node(port, server);
-		        exec.execute(worker);
-	}
-		
-	  
 	
+	executor.shutdown();
+	while (!executor.isTerminated()) {
+	}
+	System.out.println("Finished all threads");
+	
+    }
+    
+    private static void creatNode(int port, ExecutorService exec) {
+	Runnable worker = new node(port, server);
+	exec.execute(worker);
+    }    
 }
